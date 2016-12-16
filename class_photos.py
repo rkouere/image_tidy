ba11efdc1b4c5557caf8c824b771f2051c33feac
rkouere <rkouere@gmail.com>
2016-12-16 18:00:08 +0100

    creation of the folders OK

--- class_photos.py
+++ class_photos.py
@@ -3,6 +3,7 @@ import sys
 import os
 import re
 
+
 class picutre_tidy(object):
     def __init__(self, path):
         '''
@@ -11,10 +12,11 @@ class picutre_tidy(object):
         '''
         self.path = path
         self.pattern_file_name = re.compile('.*(jpg|mp4)')
-        self.pattern_get_folder_name = re.compile('IMG_(\d*)_\d*\.jpg')
+        self.pattern_get_folder_name = 'IMG_(\d*)_.*\.jpg'
         self.folder_names = []
         self.get_images()
         self.get_folder_names()
+        self.folder_out = self.path
 
     def get_images(self):
         '''
@@ -22,20 +24,29 @@ class picutre_tidy(object):
         '''
         self.pictures = os.listdir(self.path)
 
-    def _check_image_is_ok(self, name):
+    def _check_image_extention(self, name):
         '''
         Checks that the files are jpgs
         '''
         # if the filename finishes is an image/video
-        if self.pattern_file_name.match(pic) is not None:
+        if self.pattern_file_name.match(name) is not None:
             return True
 
     def get_folder_name_from_filename(self, filename):
         '''
         Extract the date from the filename.
-        Returns the file name
+        If the pattern is found: returns the file name
+        If not: returns None
         '''
-
+        if self._check_image_extention(filename):
+            m = re.search(self.pattern_get_folder_name, filename)
+            if m is not None:
+                regex_result_tmp = m.group(1)
+                date_tmp = regex_result_tmp[4:6] + "-" + regex_result_tmp[0:4]
+                if date_tmp not in self.folder_names:
+                    return date_tmp
+            else:
+                return None
 
     def get_folder_names(self):
         '''
@@ -45,17 +56,28 @@ class picutre_tidy(object):
         regex_result_tmp = ""
         date_tmp = ""
         for pic in self.pictures:
-            if self._check_image_is_ok(pic):
-                try:
-                    regex_result_tmp = self.pattern_get_folder_name.search(pic).group(1)
-                except Exception as e:
-                    print(pic)
-                    print(e)
-                date_tmp = regex_result_tmp[4:6] + "-" + regex_result_tmp[0:4]
-                if date_tmp not in self.folder_names:
-                    self.folder_names.append(date_tmp)
+            if self.get_folder_name_from_filename(pic) is not None:
+                self.folder_names.append(self.get_folder_name_from_filename(pic))
         print(self.folder_names)
 
+    def add_out_folder(self, path):
+        '''
+        Adds the path of the destination folder and creates it if needed.
+        '''
+        self.destination_folder_path = path
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def create_folders(self):
+        '''
+        Creates the folders
+        '''
+        for folder in self.folder_names:
+            print(self.path + "/" + folder)
+            if not os.path.exists(self.destination_folder_path + '/' + folder):
+                os.makedirs(self.destination_folder_path + '/' + folder)
+
+        
 
 
 #print(os.listdir("/home/rkouere/Pictures"))
@@ -67,12 +89,18 @@ def main():
     argparser = argparse.ArgumentParser(
         description=sys.modules[__name__].__doc__)
     # this is a option which need one extra argument
-    argparser.add_argument("-f",
-                           '--folder',
+    argparser.add_argument("-i",
+                           '--folder_in',
                            required=True,
                            help="The path of the folder where the pics are")
+    argparser.add_argument("-o",
+                           '--folder_out',
+                           help="The path of the folder where the pics have to be copied")
     arguments = argparser.parse_args()
-    prog = picutre_tidy(arguments.folder)
+    prog = picutre_tidy(arguments.folder_in)
+    if arguments.folder_out is not None:
+        prog.add_out_folder(arguments.folder_out)
+    prog.create_folders()
 
 # This is a Python's special:
 # The only way to tell wether we are running the program as a binary,
